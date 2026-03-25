cedff853f3e7270823f0916f67f98cc62a0eee96
Binchiz <zbinchi@outlook.com>
2026-03-25 10:15:49 +0200

    added better threshold method so there's not hardcode

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,16 @@
 #include <Arduino.h>
+#include "tests/testCycleTimeAnalog.h"
 
 void setup() {
-  // Initialization code
+    Serial.begin(9600);
+
+    TestResult r1 = testCycleTimeAnalog(2, LOW, A3, 300000, 500000);
+    Serial.println(r1.passed ? "PASS" : "FAIL");
+    Serial.println(r1.details);
+
+    TestResult r2 = testCycleTimeAnalog(2, LOW, A4, 150000, 250000);
+    Serial.println(r2.passed ? "PASS" : "FAIL");
+    Serial.println(r2.details);
 }
 
-void loop() {
-  // Main code
-}
+void loop() {}
