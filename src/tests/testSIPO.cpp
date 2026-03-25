b9f3240931e129581656b58a08a86b767078579c
carol <carolkokk@gmail.com>
2026-03-25 12:20:37 +0200

    edited order

--- src/tests/testSIPO.cpp
+++ src/tests/testSIPO.cpp
@@ -26,27 +26,14 @@ TestResult testSIPO(int dataPin, int clockPin, int latchPin, int enablePin, int 
     digitalWrite(enablePin, LOW); 
     digitalWrite(resetPin, HIGH);
 
-    //testing all off
-    sendByte(0,dataPin,clockPin,latchPin);
-    delay(10);
-
-    float voltage = analogRead(output) * (5.0 / 1024.0);
-    result.details += "ALL OFF test ";
-    if(voltage < 2.0){
-        result.details += "OK. ";
-        ++sum;
-    }else{
-        result.details += "Not OK. ";
-    }
-    result.details += "Voltage: " + String(voltage) + "\n";
 
     //testing each led separately
     for(int i = 0; i < numBits; ++i){
         int val = 1 << i;
         sendByte(val,dataPin,clockPin,latchPin);
-        delay(10);
+        delay(100);
 
-        voltage = analogRead(output) * (5.0 / 1024.0);
+        float voltage = analogRead(output) * (5.0 / 1024.0);
         result.details += "Single test: Number " + String(i);
         if(voltage > 2.0 && voltage < 3.0){
             result.details += " OK. ";
@@ -59,8 +46,8 @@ TestResult testSIPO(int dataPin, int clockPin, int latchPin, int enablePin, int 
 
     //testing all on
     sendByte((1 << numBits) - 1,dataPin,clockPin,latchPin);
-    delay(10);
-    voltage = analogRead(output) * (5.0 / 1024.0);
+    delay(100);
+    float voltage = analogRead(output) * (5.0 / 1024.0);
     result.details += "ALL ON test ";
     if(voltage > 2.5 && voltage < 3.5){
         result.details += "OK. ";
@@ -70,8 +57,24 @@ TestResult testSIPO(int dataPin, int clockPin, int latchPin, int enablePin, int 
     }
     result.details += "Voltage: " + String(voltage) + "\n";
 
+        //testing all off
+    sendByte(0,dataPin,clockPin,latchPin);
+    delay(100);
+
+    voltage = analogRead(output) * (5.0 / 1024.0);
+    result.details += "ALL OFF test ";
+    if(voltage < 2.0){
+        result.details += "OK. ";
+        ++sum;
+    }else{
+        result.details += "Not OK. ";
+    }
+    result.details += "Voltage: " + String(voltage) + "\n";
+
+
     if(sum == (numBits + 2)){
         result.passed = true;
     }
+
     return result;
 }
