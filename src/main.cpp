d97eecbca2f8e0279671d52bcd748c8d71fcca1f
Binchiz <zbinchi@outlook.com>
2026-03-25 10:21:40 +0200

    remove unused main

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,15 @@
 #include "tests/testCycleTimeAnalog.h"
 
 void setup() {
-    Serial.begin(9600);
+    // Serial.begin(9600);
 
-    TestResult r1 = testCycleTimeAnalog(2, LOW, A3, 300000, 500000);
-    Serial.println(r1.passed ? "PASS" : "FAIL");
-    Serial.println(r1.details);
+    // TestResult r1 = testCycleTimeAnalog(2, LOW, A3, 300000, 500000);
+    // Serial.println(r1.passed ? "PASS" : "FAIL");
+    // Serial.println(r1.details);
 
-    TestResult r2 = testCycleTimeAnalog(2, LOW, A4, 150000, 250000);
-    Serial.println(r2.passed ? "PASS" : "FAIL");
-    Serial.println(r2.details);
+    // TestResult r2 = testCycleTimeAnalog(2, LOW, A4, 150000, 250000);
+    // Serial.println(r2.passed ? "PASS" : "FAIL");
+    // Serial.println(r2.details);
 }
 
 void loop() {}
