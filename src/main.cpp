65885986c6127079ad9308977850ce8823f21203
Binchiz <zbinchi@outlook.com>
2026-03-25 10:34:23 +0200

    fix some compiling error

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,16 @@
 #include <Arduino.h>
-#include "tests/testCycleTimeAnalog.h"
+#include "variants/variant_serial.h"
 
-void setup() {
-    // Serial.begin(9600);
+// void setup() {
+//     // Serial.begin(9600);
 
-    // TestResult r1 = testCycleTimeAnalog(2, LOW, A3, 300000, 500000);
-    // Serial.println(r1.passed ? "PASS" : "FAIL");
-    // Serial.println(r1.details);
+//     // TestResult r1 = testCycleTimeAnalog(2, LOW, A3, 300000, 500000);
+//     // Serial.println(r1.passed ? "PASS" : "FAIL");
+//     // Serial.println(r1.details);
 
-    // TestResult r2 = testCycleTimeAnalog(2, LOW, A4, 150000, 250000);
-    // Serial.println(r2.passed ? "PASS" : "FAIL");
-    // Serial.println(r2.details);
-}
+//     // TestResult r2 = testCycleTimeAnalog(2, LOW, A4, 150000, 250000);
+//     // Serial.println(r2.passed ? "PASS" : "FAIL");
+//     // Serial.println(r2.details);
+// }
 
-void loop() {}
+// void loop() {}
